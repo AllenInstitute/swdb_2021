d664f2f2b3b2d824c7067dc985835a0ff80d4fa4
Forrest Collman <forrest.collman@gmail.com>
2021-08-23 09:36:30 -0700

    adding nb_conda_kernels to em install scripts

--- scripts/em_conda_install.bat
+++ scripts/em_conda_install.bat
@@ -1,4 +1,4 @@
-conda install -y -c conda-forge itkwidgets k3d pykdtree
+conda install -y -c conda-forge nb_conda_kernels itkwidgets k3d pykdtree
 pip install -r https://gist.githubusercontent.com/fcollman/65c23d848c3e6122958b552b1998c2e2/raw/db94cf7efd4ebbedb6f239e80e9cd643bb65075d/requirements.txt
 git clone https://github.com/scopatz/pyembree.git
 cd pyembree
--- scripts/em_conda_install.sh
+++ scripts/em_conda_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # activate your conda environment of choice first
-conda install -y -c conda-forge shapely itkwidgets k3d pykdtree pyembree
+conda install -y -c conda-forge nb_conda_kernels shapely itkwidgets k3d pykdtree pyembree
 pip install -r https://gist.githubusercontent.com/fcollman/65c23d848c3e6122958b552b1998c2e2/raw/db94cf7efd4ebbedb6f239e80e9cd643bb65075d/requirements.txt
